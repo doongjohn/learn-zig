641407fe6b7b835844c8a2ac9830e10deff991eb
doongjohn <doongjohn@gmail.com>
2025-05-28 03:36:57 +0900

    update

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "learn-zig",
+        .name = "learn_zig",
         .root_module = exe_mod,
     });
 
