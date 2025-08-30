361f8f27f9d4910cab282e20e6745f2582c8bbef
doongjohn <doongjohn@gmail.com>
2025-08-30 18:23:17 +0900

    update

--- src/hello.c
+++ src/hello.c
@@ -2,17 +2,16 @@
 
 #define BUILDING_DLL
 
-// clang-format off
 #if defined _WIN32 || defined __CYGWIN__
   #ifdef BUILDING_DLL
     #ifdef __GNUC__
-      #define DLL_PUBLIC __attribute__ ((dllexport))
+      #define DLL_PUBLIC __attribute__((dllexport))
     #else
       #define DLL_PUBLIC __declspec(dllexport)
     #endif
   #else
     #ifdef __GNUC__
-      #define DLL_PUBLIC __attribute__ ((dllimport))
+      #define DLL_PUBLIC __attribute__((dllimport))
     #else
       #define DLL_PUBLIC __declspec(dllimport)
     #endif
@@ -20,14 +19,13 @@
   #define DLL_LOCAL
 #else
   #if __GNUC__ >= 4
-    #define DLL_PUBLIC __attribute__ ((visibility ("default")))
-    #define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+    #define DLL_PUBLIC __attribute__((visibility("default")))
+    #define DLL_LOCAL __attribute__((visibility("hidden")))
   #else
     #define DLL_PUBLIC
     #define DLL_LOCAL
   #endif
 #endif
-// clang-format on
 
 DLL_PUBLIC void hello() {
   puts("hello");
